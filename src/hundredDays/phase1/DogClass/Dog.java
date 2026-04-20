2ec94cf263415b32a24b0b606f973a2a427728ff
Pietro Guidorizzi <peterguidorizzi@gmail.com>
2026-04-20 00:32:20 -0300

    Dog class Done

---
+++ src/hundredDays/phase1/DogClass/Dog.java
@@ -0,0 +1,15 @@
+package hundredDays.phase1.DogClass;
+
+public class Dog {
+    String name;
+    String breed;
+    int age;
+
+    void bark(){
+        System.out.println(this.name + " says: woof!");
+    }
+
+    void describe(){
+        System.out.println(this.name + " is a " + this.age +"-year-old " + this.breed);
+    }
+}
---
+++ src/hundredDays/phase1/DogClass/Main.java
@@ -0,0 +1,16 @@
+package hundredDays.phase1.DogClass;
+
+public class Main {
+   static void main(String[] args){
+       Dog rex = new Dog();
+       rex.name = "Rex";
+       rex.breed = "Pit-Bull";
+       rex.age = 12;
+       rex.bark();
+       rex.describe();
+   }
+
+
+
+
+}
